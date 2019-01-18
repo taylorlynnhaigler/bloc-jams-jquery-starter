83ab7524d9d3dd5408df3d6daadbf7c880599794
tay <taylorlynn@haigler.net>
2019-01-18 14:10:17 -0600

    assignment 10 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -18,8 +18,9 @@
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex <= 1) { return; }
+    if (currentSongIndex < 1) { return; }
 
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
