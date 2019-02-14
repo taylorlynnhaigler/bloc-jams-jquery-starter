3f91170555e26d573e560779d35f4e632eed3ff9
tay <taylorlynn@haigler.net>
2019-02-14 11:29:04 -0600

    Assignment 11 updates

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,21 @@
+// $(“#time-control .total-time").addClass("helper");
+//
+// class Helper{
+//   constructor(){
+//     // your stuff here
+//   }
+//   playPauseAndUpdate(song){
+//     playPause (song = this.currentlyPlaying);
+//   }
+// }
+// const helper = newHelper();
 
-$(“#time-control .total-time").addClass("helper");
 
-const helper = player.playPauseAndUpdate(song) {
-  player.playPause(song);
-  const totalTime = player.getDuration();
-  $('#time-control .total-time').text(player.prettyTime(totalTime));
-}
+
+
+// first try --
+// const helper = player.playPauseAndUpdate(song) {
+//   player.playPause(song);
+//   const totalTime = player.getDuration();
+//   $('#time-control .total-time').text(player.prettyTime(totalTime));
+// }
